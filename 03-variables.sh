46e0376108f4d21ba19c950dab7739d31a640886
NArunkumar <nelluri.arunkumar@gmail.com>
2025-05-21 17:59:46 +0530

    Shell scripting started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Modi   #There should be no space before and after '='
+PERSON2=Musk
+
+echo "$PERSON1: Hey $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I'm Fine. How are you doing?"
+echo "$PERSON1: I'm fine too, what's up?"
+echo "$PERSON2: Nothing, just going to Mars now, will you come?"
+echo "$PERSON1: Sorry, you carry on! I will come once you come back"

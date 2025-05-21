e62f4585c702bf607bda37276c05f1c55fa9730c
NArunkumar <nelluri.arunkumar@gmail.com>
2025-05-21 18:28:29 +0530

    Shell scripting started

---
+++ 06-data-types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+
+
+TIMESTAMP=$(date)
+
+echo "Script execute at : $TIMESTAMP"
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Sum of $NUMBER1 and $NUMBER2 is : $SUM"

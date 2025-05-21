9d363d2ecc1f6d084351505573a7db62eab445ae
NArunkumar <nelluri.arunkumar@gmail.com>
2025-05-21 18:43:24 +0530

    Shell scripting started

---
+++ 07-array.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##In this script we see the array data type.. 
+##We can mention the multiple values in single varible with space as a separator known as array..
+##We usually mention all values in double quotes inside the braces..
+##In array data type the indexing start at 0... 
+##So the first value is point to 0 and second value is pointing to 1 and so on....
+
+
+MOVIES=("Court" "HIT3" "PUSHPA2" "THANDEL")
+
+
+echo "First Movie: ${MOVIES[0]}"
+echo "Third Movie: ${MOVIES[2]}"
+
+
+echo "All Movie: ${MOVIES[@]}" ##When we need to print all values of the array we usually use @ symbol...
+
+##Here $varible == ${variable} both are equal only 

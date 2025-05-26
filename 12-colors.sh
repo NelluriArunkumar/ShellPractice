154beb93e426cf6ea762d7091420215d09cae73d
NArunkumar <nelluri.arunkumar@gmail.com>
2025-05-26 18:52:55 +0530

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -5,6 +5,10 @@
 ##Here 0 denotes no color
 ##31--> red ...32 --> Green... 33-->Yellow.. we ned need to change the number accordingly....
 
-echo -e "\e[31m Hello color"
+echo -e "\e[31m Hello color" 
 
-echo -e "Hello no color"
+## "-e" is enabling the color
+## "/e[0m" is used to end the color till that line...
+
+echo -e "Hello no color" ##If we doesn't end the color after denoting it will apply entire script..
+

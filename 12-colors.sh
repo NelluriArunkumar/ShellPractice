d90177899051ea6715ca36f6af9ad6beac050b28
NArunkumar <nelluri.arunkumar@gmail.com>
2025-05-26 18:41:15 +0530

    colors

---
+++ 12-colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##We are discussing the colors in this script
+##the syntax for applying the color to font is "\e[0m"
+##Here 0 denotes no color
+##31--> red ...32 --> Green... 33-->Yellow.. we ned need to change the number accordingly....
+
+echo -e "\e[31m Hello color"
+
+echo -e "Hello no color"

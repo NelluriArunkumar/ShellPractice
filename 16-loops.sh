3aba36f2e7f4004986780491130bc66b71b6084f
NArunkumar <nelluri.arunkumar@gmail.com>
2025-06-17 17:51:39 +0530

    Loops

--- 16-loops.sh
+++ 16-loops.sh
@@ -40,7 +40,8 @@ VALIDATE(){
         exit 1
     fi
 }
-for package in ${PACKAGES[@]}
+#for package in ${PACKAGES[@]}
+for package in $@   ##This will read the package name from the command line which means dynamically...
 do
     dnf list installed $package &>>$LOG_FILE
 

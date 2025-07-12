11eef46c227ba4006da67e4f959a040cff16b3cc
NArunkumar <nelluri.arunkumar@gmail.com>
2025-07-12 19:07:48 +0530

    Shell practice

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -17,4 +17,4 @@ done <<< $DISK_USAGE
 
 #echo -e $MSG
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "arunkumar.jun15@gmail.com" "ALERT-High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "YourMail@gmail.com" "ALERT-High Disk Usage"
